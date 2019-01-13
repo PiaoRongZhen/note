fd2c445c979d5efef2a5634b4fd4f26eb72be594
PiaoRongZhen <947418664@qq.com>
2019-01-13 15:02:53 +0800

    Update MessageController.java

--- Spring/SpringMVC/Demo/src/demo/web/MessageController.java
+++ Spring/SpringMVC/Demo/src/demo/web/MessageController.java
@@ -14,8 +14,7 @@ public class MessageController {
     private MessageRepository messageRepository;
 
     @RequestMapping(value="/message", method=org.springframework.web.bind.annotation.RequestMethod.GET)
-    public String message(@RequestParam(value="count", defaultValue="20") int count,
-                          Model model) {
+    public String message(@RequestParam(value="count", defaultValue="20") int count, Model model) {
         model.addAttribute("messageList", messageRepository.findMessage(count));
         return "message";
     }
